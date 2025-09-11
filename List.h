83360abc1ab42ae66f64afa867b963dc101ce5c7
ran-research-lab <rafael.arce@upr.edu>
2025-09-11 00:38:44 +0000

    removed unnnecesary code

--- List.h
+++ List.h
@@ -10,32 +10,9 @@ using namespace std;
 class List {
 private:
     int *a;
-    int cap;
     int size;
-    
-    void expand();
-    
+    int cap;
 public:
-    List();
-
-    List(const List &rSide);
-
-    List(List &&rSide);
-    
-    const List& operator=(const List &rSide);
-
-    List& operator=(List &&rSide);
-
-    List operator+(const List &rsOp);
-
-    ~List();
-
-    void append(int n);
-
-    bool operator==(const List &rSideOp) const ;
-    int getSize() const ;
-    string toString() const;
-    
 };
 
 #endif
--- main.cpp
+++ main.cpp
@@ -1,66 +1,14 @@
 // Gran cantidad de este código fue generado por chatGPT
 // Yo lo adapté para simplificarlo para la clase
 
-#include <iostream>
-#include <vector>
-#include <unordered_set>
-#include <string>
-#include <fstream>
-#include <chrono>
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+
+#include "doctest.h"
+
+#include "List.h"
 
 using namespace std;
 
-class List {
-private:
-    int *a;
-public:
-    List() {
-        cout << "\tCalling the default constructor\n";
-    }
-    List(List &rSide) {
-        cout << "\tCalling the copy constructor\n";
-    }
-
-    List(List &&rSide) {
-        cout << "\tCalling the copy move constructor\n";
-    }
-
-    List& operator=(List &rSide) {
-        cout << "\tCalling the assignment operator\n";
-        return rSide;
-    }
-
-    List& operator=(List &&rSide) {
-        cout << "\tCalling the move assignment operator\n";
-        return rSide;
-    }
-
-    List operator+(const List &rsOp) {
-        List res;
-        return res;
-    }
-};
-
-
-int main() {
-    cout << "For this declaration: List L;" << endl;
-    List L;
-
-    cout << "For this declaration: List M(L);" << endl;
-    List M(L);
-
-    cout << "For this declaration: List N = std::move(L);" << endl;
-    List N = std::move(L);
-
-    cout << "Creating another object with: List P;" << endl;
-    List P;
-
-    cout << "Assigning with this expression: P = L;" << endl;
-    P = L;
-
-    cout << "Assigning with this expression: P = std::move(N);" << endl;
-    P = std::move(N);
-
-    P = N + P;
-    return 0;
+TEST_CASE("Test default constructor") {
+    CHECK_NOTHROW(List());
 }
